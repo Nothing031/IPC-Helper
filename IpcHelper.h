7cf5c5213323bdd090cc86c1bd65e9deaa32f130
Nothing031 <160526040+Nothing031@users.noreply.github.com>
2025-07-19 23:11:20 +0900

    Update IpcHelper.h

--- IpcHelper.h
+++ IpcHelper.h
@@ -1,156 +1,300 @@
 #pragma once
 #include <memory>
 #include <string>
+#include <ctime>
 #include <Windows.h>
 
-class IpcHelper
+namespace IpcHelper
 {
-public:
-	class Sender;
-	class Reader;
-};
+	enum EWaitResult {
+		IPC_WAIT_TIMEOUT,
+		IPC_WAIT_FAILED,
+		IPC_WAIT_SUCCESS,
+		IPC_WAIT_QUIT,
+		IPC_WAIT_SHUTDOWN,
+	};
 
-class IpcHelper::Sender
-{
-public:
-	inline static bool Init(const char* name, int mapSize)
+	class SharedMemoryHelper
 	{
-		if (initialized) return true;
-		size = mapSize;
-		std::string mutexName = std::string(name) + ".mutex";
-		std::string eventName = std::string(name) + ".event";
-		std::string mapFileName = std::string(name) + ".mapfile";
+	public:
+		~SharedMemoryHelper();
+		SharedMemoryHelper(const SharedMemoryHelper&) = delete;
+		SharedMemoryHelper& operator=(const SharedMemoryHelper&) = delete;
 
-		hMutex = CreateMutexA(NULL, FALSE, mutexName.c_str());
-		hEvent = CreateEventA(NULL, TRUE, FALSE, eventName.c_str());
-		HANDLE hMapFile = CreateFileMappingA(
-			INVALID_HANDLE_VALUE,
-			NULL,
-			PAGE_READWRITE,
-			0,
-			size,
-			mapFileName.c_str()
-		);
-		if (hMutex == INVALID_HANDLE_VALUE || hEvent == INVALID_HANDLE_VALUE || hMapFile == INVALID_HANDLE_VALUE)
-			return false;
-		
-		pBuffer = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, size);
-		if (pBuffer == NULL)
-			return false;
-		ZeroMemory(pBuffer, size);
+		static bool isExist(std::wstring);
+		static bool openSharedMemory(std::wstring str, uint32_t size, std::unique_ptr<SharedMemoryHelper>* helperOut);
+		static bool createSharedMemory(std::wstring str, uint32_t size, std::unique_ptr<SharedMemoryHelper>* helperOut);
 
-		initialized = true;
+		// info
+		int size();
+
+		// synchronous
+		void quitWaiting();
+		void lock();
+		void unlock();
+
+		// motify
+		IpcHelper::EWaitResult waitWriteData(void* dst, uint32_t dstSize, uint32_t timeout = INFINITE);
+		IpcHelper::EWaitResult waitReadData(void* src, uint32_t srcSize, uint32_t timeout = INFINITE);
+
+		// data
+		void* rawBuffer();
+	protected:
+		SharedMemoryHelper();
+
+	private:
+		void CleanUp();
+
+		bool m_initialized;
+		int m_size;
+		LPVOID m_pBuffer;
+		HANDLE m_hBufferMutex;
+		HANDLE m_hBufferEvent;
+		HANDLE m_hBufferMapFile;
+		HANDLE m_hShutdownEvent;
+		HANDLE m_hInstanceQuitWaitingEvent;
+	};
+}
+
+bool IpcHelper::SharedMemoryHelper::isExist(std::wstring str)
+{
+	HANDLE mutex = OpenMutexW(SYNCHRONIZE | MUTEX_ALL_ACCESS, NULL, std::wstring(str + L".buffer.mutex").c_str());
+	if (mutex)
+	{
+		CloseHandle(mutex);
 		return true;
 	}
-
-	inline static int Size()
+	else
 	{
-		if (!initialized) [[unlikely]] return NULL;
-		return size;
+		return false;
 	}
+}
 
-	inline static LPVOID Buffer()
-	{
-		if (!initialized) [[unlikely]] return NULL;
-		return pBuffer;
-	}
-
-	inline static void SendData(void* src, int srcSize)
-	{
-		if (!initialized || !src) [[unlikely]] return;
-
-		WaitForSingleObject(hMutex, INFINITE);
-		memcpy(pBuffer, src, srcSize);
-		SetEvent(hEvent);
-		ReleaseMutex(hMutex);
-	}
-private:
-	static bool initialized;
-	static int size;
-	static LPVOID pBuffer;
-	static HANDLE hEvent;
-	static HANDLE hMutex;
-	static HANDLE hMapFile;
-};
-
-class IpcHelper::Reader
+bool IpcHelper::SharedMemoryHelper::openSharedMemory(std::wstring str, uint32_t size, std::unique_ptr<SharedMemoryHelper>* helperOut)
 {
-public:
-	inline static bool Init(const char* name, int mapSize)
+	std::unique_ptr<SharedMemoryHelper> helper(new SharedMemoryHelper());
+	std::wstring mapFileName = std::wstring(str) + L".buffer.mapfile";
+	std::wstring bufferMutexName = std::wstring(str) + L".buffer.mutex";
+	std::wstring bufferEventName = std::wstring(str) + L".buffer.event.data";
+	std::wstring shutdownName = std::wstring(str) + L".buffer.event.shutdown";
+	// mapfile
+	helper->m_hBufferMapFile = OpenFileMappingW(
+		FILE_MAP_ALL_ACCESS,
+		FALSE,
+		mapFileName.c_str()
+	);
+	if (helper->m_hBufferMapFile == NULL)
+		goto Failed;
+	// buffer
+	helper->m_pBuffer = MapViewOfFile(helper->m_hBufferMapFile, FILE_MAP_ALL_ACCESS, 0, 0, size);
+	if (helper->m_pBuffer == NULL)
+		goto Failed;
+	// buffer mutex, event
+	helper->m_hBufferMutex = OpenMutexW(MUTEX_ALL_ACCESS, FALSE, bufferMutexName.c_str());
+	helper->m_hBufferEvent = OpenEventW(EVENT_ALL_ACCESS, FALSE, bufferEventName.c_str());
+	// shutdown event
+	helper->m_hShutdownEvent = OpenEventW(EVENT_ALL_ACCESS, FALSE, shutdownName.c_str());
+	if (helper->m_hBufferMutex == NULL || helper->m_hBufferEvent == NULL || helper->m_hShutdownEvent == NULL)
+		goto Failed;
+
+Success:
+	helper->m_initialized = true;
+	(*helperOut) = std::move(helper);
+	return true;
+
+Failed:
+	(*helperOut) = nullptr;
+	return false;
+}
+
+bool IpcHelper::SharedMemoryHelper::createSharedMemory(std::wstring str, uint32_t size, std::unique_ptr<SharedMemoryHelper>* helperOut)
+{
+	std::unique_ptr<SharedMemoryHelper> helper(new SharedMemoryHelper());
+	std::wstring mapFileName = std::wstring(str) + L".buffer.mapfile";
+	std::wstring bufferMutexName = std::wstring(str) + L".buffer.mutex";
+	std::wstring bufferEventName = std::wstring(str) + L".buffer.event.data";
+	std::wstring shutdownName = std::wstring(str) + L".buffer.event.shutdown";
+
+	if (isExist(str))
+		goto Failed;
+
+	helper->m_hBufferMapFile = CreateFileMappingW(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		size,
+		mapFileName.c_str()
+	);
+	if (helper->m_hBufferMapFile == NULL || GetLastError() == ERROR_ALREADY_EXISTS)
+		goto Failed;
+	// buffer
+	helper->m_pBuffer = MapViewOfFile(helper->m_hBufferMapFile, FILE_MAP_ALL_ACCESS, 0, 0, size);
+	if (helper->m_pBuffer == NULL)
+		goto Failed;
+	// buffer mutex, evvent
+	helper->m_hBufferMutex = CreateMutexW(NULL, FALSE, bufferMutexName.c_str());
+	if (helper->m_hBufferMutex == NULL || GetLastError() == ERROR_ALREADY_EXISTS)
+		goto Failed;
+	helper->m_hBufferEvent = CreateEventW(NULL, FALSE, FALSE, bufferEventName.c_str());
+	if (helper->m_hBufferEvent == NULL || GetLastError() == ERROR_ALREADY_EXISTS)
+		goto Failed;
+	// shutdown event
+	helper->m_hShutdownEvent = CreateEventW(NULL, FALSE, FALSE, shutdownName.c_str());
+	if (helper->m_hShutdownEvent == NULL || GetLastError() == ERROR_ALREADY_EXISTS)
+		goto Failed;
+
+Success:
+	helper->m_initialized = true;
+	(*helperOut) = std::move(helper);
+	return true;
+
+Failed:
+	(*helperOut) = nullptr;
+	return false;
+}
+
+void IpcHelper::SharedMemoryHelper::quitWaiting()
+{
+	SetEvent(m_hInstanceQuitWaitingEvent);
+}
+
+void IpcHelper::SharedMemoryHelper::lock()
+{
+	WaitForSingleObject(m_hBufferMutex, INFINITE);
+}
+
+void IpcHelper::SharedMemoryHelper::unlock()
+{
+	ReleaseMutex(m_hBufferMutex);
+}
+
+IpcHelper::EWaitResult IpcHelper::SharedMemoryHelper::waitWriteData(void* src, uint32_t srcSize, uint32_t timeout)
+{
+	if (!m_initialized) [[unlikely]]
+		return EWaitResult::IPC_WAIT_FAILED;
+
+	HANDLE waitHandles[2] = { m_hBufferMutex, m_hInstanceQuitWaitingEvent };
+	DWORD dwResult = WaitForMultipleObjects(2, waitHandles, FALSE, timeout);
+	switch (dwResult)
 	{
-		if (initialized) return true;
-		size = mapSize;
-		std::string mutexName = std::string(name) + ".mutex";
-		std::string eventName = std::string(name) + ".event";
-		std::string mapFileName = std::string(name) + ".mapfile";
-
-		hMutex = OpenMutexA(SYNCHRONIZE | EVENT_ALL_ACCESS, FALSE, mutexName.c_str());
-		hEvent = OpenEventA(SYNCHRONIZE | EVENT_ALL_ACCESS, FALSE, eventName.c_str());
-
-		HANDLE hMapFile = OpenFileMappingA(
-			FILE_MAP_ALL_ACCESS,
-			FALSE,
-			mapFileName.c_str()
-		);
-
-		if (hMutex == INVALID_HANDLE_VALUE || hEvent == INVALID_HANDLE_VALUE || hMapFile == INVALID_HANDLE_VALUE)
-			return false;
-
-		pBuffer = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, size);
-		if (pBuffer == NULL)
-			return false;
-
-		initialized = true;
-		return true;
+	case WAIT_OBJECT_0:		// got mutex
+		memcpy(m_pBuffer, src, srcSize);
+		SetEvent(m_hBufferEvent);
+		ReleaseMutex(m_hBufferMutex);
+		return EWaitResult::IPC_WAIT_SUCCESS;
+	case WAIT_OBJECT_0 + 1:	// quit waiting
+		ResetEvent(m_hInstanceQuitWaitingEvent);
+		return EWaitResult::IPC_WAIT_QUIT;
+	case WAIT_TIMEOUT:
+		return EWaitResult::IPC_WAIT_TIMEOUT;
+	default:
+		return EWaitResult::IPC_WAIT_FAILED;
 	}
+	return EWaitResult::IPC_WAIT_FAILED;
+}
 
-	inline static void QuitWaiting()
+IpcHelper::EWaitResult IpcHelper::SharedMemoryHelper::waitReadData(void* dst, uint32_t dstSize, uint32_t timeout)
+{
+	if (!m_initialized) [[unlikely]]
+		return EWaitResult::IPC_WAIT_FAILED;
+
+	uint32_t waitBeginTime, waitElapsedTime, nextWaitTime;
+	waitBeginTime = clock();
+	HANDLE waitHandles[2] = { m_hBufferEvent, m_hInstanceQuitWaitingEvent };
+	DWORD dwResult = WaitForMultipleObjects(2, waitHandles, FALSE, timeout);
+	waitElapsedTime = clock() - waitBeginTime;
+	nextWaitTime = timeout - waitElapsedTime > 0
+		? timeout - waitElapsedTime
+		: 0;
+
+	EWaitResult waitResult = EWaitResult::IPC_WAIT_FAILED;
+	switch (dwResult)
 	{
-		if (!initialized) [[unlikely]] return;
-
-		SetEvent(hEvent);
-	}
-
-	inline static void WaitAndReadData(void* dest, int destSize)
+	case WAIT_OBJECT_0:		// data
 	{
-		if (!initialized || !dest) [[unlikely]] return;
-		
-		WaitForSingleObject(hEvent, INFINITE);
-		WaitForSingleObject(hMutex, INFINITE);
-		memcpy(dest, pBuffer, destSize);
-		ResetEvent(hEvent);
-		ReleaseMutex(hMutex);
+		ResetEvent(m_hBufferEvent);
+		HANDLE waitHandles2[2] = { m_hBufferMutex, m_hInstanceQuitWaitingEvent };
+		DWORD dwResult2 = WaitForMultipleObjects(2, waitHandles2, FALSE, timeout);
+		switch (dwResult2)
+		{
+		case WAIT_OBJECT_0: // got event
+			memcpy(dst, m_pBuffer, dstSize);
+			ReleaseMutex(m_hBufferMutex);
+			waitResult = EWaitResult::IPC_WAIT_SUCCESS;
+			break;
+		case WAIT_OBJECT_0 + 1: // quit waiting
+			ResetEvent(m_hInstanceQuitWaitingEvent);
+			waitResult = EWaitResult::IPC_WAIT_QUIT;
+			break;
+		case WAIT_TIMEOUT:
+			waitResult = EWaitResult::IPC_WAIT_TIMEOUT;
+			break;
+		}
+		break;
 	}
+	case WAIT_OBJECT_0 + 1: // quit waiting
+		ResetEvent(m_hInstanceQuitWaitingEvent);
+		waitResult =  EWaitResult::IPC_WAIT_QUIT;
+		break;
+	case WAIT_TIMEOUT:
+		waitResult = EWaitResult::IPC_WAIT_TIMEOUT;
+		break;
+	}
+	return waitResult;
+}
 
-	inline static int Size()
+void* IpcHelper::SharedMemoryHelper::rawBuffer()
+{
+	return m_pBuffer;
+}
+
+IpcHelper::SharedMemoryHelper::SharedMemoryHelper()
+	: m_initialized(false)
+	, m_size(0)
+	, m_pBuffer(NULL)
+	, m_hBufferEvent(NULL)
+	, m_hBufferMutex(NULL)
+	, m_hBufferMapFile(NULL)
+	, m_hShutdownEvent(NULL)
+	, m_hInstanceQuitWaitingEvent(CreateEvent(NULL, TRUE, FALSE, NULL))
+{
+}
+
+IpcHelper::SharedMemoryHelper::~SharedMemoryHelper()
+{
+	CleanUp();
+}
+
+void IpcHelper::SharedMemoryHelper::CleanUp()
+{
+	m_initialized = false;
+	m_size = 0;
+	m_pBuffer = NULL;
+
+	if (m_hBufferMutex)
 	{
-		size;
+		CloseHandle(m_hBufferMutex);
+		m_hBufferMutex = NULL;
 	}
-
-	inline static LPVOID Buffer()
+	if (m_hBufferEvent)
 	{
-		pBuffer;
+		CloseHandle(m_hBufferEvent);
+		m_hBufferEvent = NULL;
 	}
-
-private:
-	static bool initialized;
-	static int size;
-	static LPVOID pBuffer;
-	static HANDLE hEvent;
-	static HANDLE hMutex;
-	static HANDLE hMapFile;
-};
-
-bool IpcHelper::Sender::initialized = false;
-int IpcHelper::Sender::size = 0;
-LPVOID IpcHelper::Sender::pBuffer = NULL;
-HANDLE IpcHelper::Sender::hEvent = INVALID_HANDLE_VALUE;
-HANDLE IpcHelper::Sender::hMutex = INVALID_HANDLE_VALUE;
-HANDLE IpcHelper::Sender::hMapFile = INVALID_HANDLE_VALUE;
-
-bool IpcHelper::Reader::initialized = false;
-int IpcHelper::Reader::size = 0;
-LPVOID IpcHelper::Reader::pBuffer = NULL;
-HANDLE IpcHelper::Reader::hEvent = INVALID_HANDLE_VALUE;
-HANDLE IpcHelper::Reader::hMutex = INVALID_HANDLE_VALUE;
-HANDLE IpcHelper::Reader::hMapFile = INVALID_HANDLE_VALUE;
+	if (m_hBufferMapFile)
+	{
+		CloseHandle(m_hBufferMapFile);
+		m_hBufferMapFile = NULL;
+	}
+	if (m_hShutdownEvent)
+	{
+		CloseHandle(m_hShutdownEvent);
+		m_hShutdownEvent = NULL;
+	}
+	if (m_hInstanceQuitWaitingEvent)
+	{
+		CloseHandle(m_hInstanceQuitWaitingEvent);
+		m_hInstanceQuitWaitingEvent = NULL;
+	}
+}
